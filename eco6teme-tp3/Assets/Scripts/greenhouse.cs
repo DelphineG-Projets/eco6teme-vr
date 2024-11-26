0d994ecca318bd8247753eae17423291058f8eed
Delphine <delphinegrenier98@gmail.com>
2024-11-25 20:28:39 -0500

    changement plants

--- eco6teme-tp3/Assets/Scripts/greenhouse.cs
+++ eco6teme-tp3/Assets/Scripts/greenhouse.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class greenhouse : MonoBehaviour
+public class Greenhouse : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Lights indicating puzzle progress
+    public GameObject light_teller_green_1;
+    public GameObject light_teller_green_2;
+    public GameObject light_teller_green_3;
+    public GameObject light_teller_red_1;
+    public GameObject light_teller_red_2;
+    public GameObject light_teller_red_3;
 
-    // Update is called once per frame
+    // Plant stages
+    public GameObject plante_3_etape_1;
+    public GameObject plante_3_etape_2;
+    public GameObject plante_3_etape_3;
+
     void Update()
     {
-        
+        if (plante_3_etape_1.activeSelf)
+        {
+            light_teller_green_1.SetActive(true);
+            light_teller_red_1.SetActive(false);
+        }
+        if (plante_3_etape_2.activeSelf)
+        {
+            light_teller_green_2.SetActive(true);
+            light_teller_red_2.SetActive(false);
+        }
+        if (plante_3_etape_3.activeSelf)
+        {
+            light_teller_green_3.SetActive(true);
+            light_teller_red_3.SetActive(false);
+        }
     }
 }
