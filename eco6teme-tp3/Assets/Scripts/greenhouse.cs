df4b3bf74994badf6b738d603ed8fe246ec29dd8
Delphine <112188978+DelphineG-Projets@users.noreply.github.com>
2024-11-26 11:08:36 -0500

    fix script

--- eco6teme-tp3/Assets/Scripts/greenhouse.cs
+++ eco6teme-tp3/Assets/Scripts/greenhouse.cs
@@ -5,51 +5,62 @@ using UnityEngine;
 public class Greenhouse : MonoBehaviour
 {
 
-    // Sockets
+
+    public bool watercanOn;
+    public bool waterOn; //Pressed the btn
+
+
+
+
+    /*  // Plant 1 stages
+     *      // Sockets
     public GameObject balance_interactable;
     public GameObject waterpump_interactable;
 
     // Watercan
-    public GameObject watercan;
+    public LayerMask watercan = LayerMask.GetMask("Wall");
     public GameObject particles;
-    public bool watercanOn;
+     public GameObject plante_1_etape_0;
+     public GameObject plante_1_etape_1;
+     public GameObject plante_1_etape_2;
+     public GameObject plante_1_etape_3;
 
+     // Plant 2 stages
+     public GameObject plante_2_etape_0;
+     public GameObject plante_2_etape_1;
+     public GameObject plante_2_etape_2;
+     public GameObject plante_2_etape_3;
 
-    // Plant 1 stages
-    public GameObject plante_1_etape_0;
-    public GameObject plante_1_etape_1;
-    public GameObject plante_1_etape_2;
-    public GameObject plante_1_etape_3;
+     // Plant 3 stages
+     public GameObject plante_3_etape_0;
+     public GameObject plante_3_etape_1;
+     public GameObject plante_3_etape_2;
+     public GameObject plante_3_etape_3;
 
-    // Plant 2 stages
-    public GameObject plante_2_etape_0;
-    public GameObject plante_2_etape_1;
-    public GameObject plante_2_etape_2;
-    public GameObject plante_2_etape_3;
-
-    // Plant 3 stages
-    public GameObject plante_3_etape_0;
-    public GameObject plante_3_etape_1;
-    public GameObject plante_3_etape_2;
-    public GameObject plante_3_etape_3;
-
-    // Lumières puzzle progression
+     // Lumières puzzle progression
     public GameObject light_teller_green_1;
-    public GameObject light_teller_green_2;
-    public GameObject light_teller_green_3;
-    public GameObject light_teller_red_1;
-    public GameObject light_teller_red_2;
-    public GameObject light_teller_red_3;
+     public GameObject light_teller_green_2;
+     public GameObject light_teller_green_3;
+     public GameObject light_teller_red_1;
+     public GameObject light_teller_red_2;
+     public GameObject light_teller_red_3;*/
 
+    public void waterTrue()
+    {
+        watercanOn = true;
+    }
 
-
-   public void WatercanActivate()
+    public void waterFalse()
     {
         watercanOn = false;
-       if (particles && )
-        {
-            watercanOn = true;
-        }
-
     }
+
+    public void btnwaterOn()
+    {
+        if (watercanOn)
+        {
+            particleswater.SetActive(true);
+        }
+    }
+
 }
