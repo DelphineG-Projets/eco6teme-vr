255297c0ef859f2af59e104424b0815ee05a84b0
Delphine <delphinegrenier98@gmail.com>
2024-11-26 20:30:30 -0500

    select script

--- eco6teme-tp3/Assets/Scripts/greenhouse.cs
+++ eco6teme-tp3/Assets/Scripts/greenhouse.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Greenhouse : MonoBehaviour
 {
-
     public XRSocketInteractor balanceSocket;
 
     public bool watercanOn;
-    public bool waterOn; //Pressed the btn
-    public bool stepOne; 
-    public bool stepTwo; 
+    public bool waterOn; // Pressed the btn
+    public bool stepOne;
+    public bool stepTwo;
 
     public GameObject particleswater;
     public GameObject btnWater;
@@ -27,42 +27,28 @@ public class Greenhouse : MonoBehaviour
     public GameObject light_teller_green_3;
     public GameObject light_teller_red_3;
 
-
-    /*  // Plant 1 stages
-     *      // Sockets
-    public GameObject balance_interactable;
-    public GameObject waterpump_interactable;
-
-    // Watercan
-    public LayerMask watercan = LayerMask.GetMask("Wall");
-    public GameObject particles;
-     public GameObject plante_1_etape_0;
-     public GameObject plante_1_etape_1;
-     public GameObject plante_1_etape_2;
-     public GameObject plante_1_etape_3;
-
-     // Plant 2 stages
-     public GameObject plante_2_etape_0;
-     public GameObject plante_2_etape_1;
-     public GameObject plante_2_etape_2;
-     public GameObject plante_2_etape_3;
-
-     // Plant 3 stages
-     public GameObject plante_3_etape_0;
-     public GameObject plante_3_etape_1;
-     public GameObject plante_3_etape_2;
-     public GameObject plante_3_etape_3;
-
-     // Lumières puzzle progression
-    public GameObject light_teller_green_1
-     public GameObject light_teller_green_2;
-     public GameObject light_teller_green_3;
-     public GameObject light_teller_red_1
-     public GameObject light_teller_red_2;
-     public GameObject light_teller_red_3;*/
+    public GameObject plante_1_etape_0;
+    public GameObject plante_1_etape_1;
+    public GameObject plante_1_etape_2;
+    public GameObject plante_1_etape_3;
 
 
-    //WATER STEP 1
+    XRSocketInteractor socket;
+
+    void Start()
+    {
+        socket = GetComponent<XRSocketInteractor>();
+    }
+
+    public void socketCheck()
+    {
+
+        IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
+
+        Debug.Log(objName.transform.name + " in socket of " + transform.name);
+    }
+
+    // Water STEP 1
     public void waterTrue()
     {
         watercanOn = true;
@@ -81,34 +67,32 @@ public class Greenhouse : MonoBehaviour
         }
     }
 
-
     public void balance()
     {
-        if ((balanceSocket.interactionLayers & InteractionLayerMask.GetMask("Arrosoir")) != 0 & waterOn)
+        if (((balanceSocket.interactionLayers & InteractionLayerMask.GetMask("Arrosoir")) != 0) && waterOn)
         {
             stepOne = true;
-            light_teller_green_1.SetActive(true);
-            light_teller_red_1.SetActive(false);
+            plante_1_etape_0.SetActive(false);
+            plante_1_etape_1.SetActive(true);
             waterOn = false;
         }
 
-        if ((balanceSocket.interactionLayers & InteractionLayerMask.GetMask("engrais_vert")) != 0 & stepOne)
+        if (((balanceSocket.interactionLayers & InteractionLayerMask.GetMask("engrais_vert")) != 0) && stepOne)
         {
             stepTwo = true;
-            light_teller_green_2.SetActive(true);
-            light_teller_red_2.SetActive(false);
+            plante_1_etape_1.SetActive(false);
+            plante_1_etape_2.SetActive(true);
         }
-
     }
 
     public void Light()
     {
         if (stepTwo)
         {
-            light_teller_green_3.SetActive(true);
-            light_teller_red_3.SetActive(false);
+            plante_1_etape_2.SetActive(false);
+            plante_1_etape_3.SetActive(true);
+            light_teller_green_1.SetActive(true);
+            light_teller_red_1.SetActive(false);
         }
     }
-    
-
 }
