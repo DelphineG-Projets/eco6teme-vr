4e8302aafe6835d3b1a5c92a935d55ddbb63405e
Delphine <delphinegrenier98@gmail.com>
2024-12-01 20:22:19 -0500

    script greenhouse termine

--- eco6teme-tp3/Assets/Scripts/greenhouse.cs
+++ eco6teme-tp3/Assets/Scripts/greenhouse.cs
@@ -1,33 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Net.Sockets;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 public class Greenhouse : MonoBehaviour
 {
     public XRSocketInteractor balanceSocket;
-
     public bool watercanOn;
-    public bool waterOn; // Pressed the btn
+    public bool waterOn;
     public bool stepZeroPlant;
     public bool stepOnePlant;
     public bool stepTwoPlant;
     public bool stepThreePlant;
-
     public GameObject particleswater;
     public GameObject btnWater;
 
     public bool plant1;
     public bool plant2;
     public bool plant3;
-    public bool light;
 
     public GameObject light_red;
     public GameObject light_green;
-    public bool lightchange;
 
+
+    public bool light_square;
+    public bool lightchange;
     public GameObject light_teller_green_1;
     public GameObject light_teller_red_1;
     public GameObject light_teller_green_2;
@@ -45,7 +42,10 @@ public class Greenhouse : MonoBehaviour
     public GameObject plante_2_etape_2;
     public GameObject plante_2_etape_3;
 
-
+    public GameObject plante_3_etape_0;
+    public GameObject plante_3_etape_1;
+    public GameObject plante_3_etape_2;
+    public GameObject plante_3_etape_3;
 
     XRSocketInteractor socket;
 
@@ -53,19 +53,16 @@ public class Greenhouse : MonoBehaviour
     {
         socket = GetComponent<XRSocketInteractor>();
         stepZeroPlant = true;
-        light = false;
+        light_square = false;
         lightchange = false;
     }
 
     public void socketCheck()
     {
-
         IXRSelectInteractable objName = socket.GetOldestInteractableSelected();
-
         Debug.Log(objName.transform.name + " in socket of " + transform.name);
     }
 
-    // Water STEP 1
     public void waterTrue()
     {
         watercanOn = true;
@@ -86,15 +83,11 @@ public class Greenhouse : MonoBehaviour
 
     public void OnSelectEntered(SelectEnterEventArgs args)
     {
-        // Récupérer le GameObject de l'objet sélectionné
         GameObject selectedObject = args.interactableObject.transform.gameObject;
-
-        // Afficher le tag de l'objet placé sur le socket
         Debug.Log("Objet placé sur le socket a le tag : " + selectedObject.tag);
 
         if (stepZeroPlant)
         {
-            // Vérifier si l'objet a le tag "Arrosoir"
             if (selectedObject.CompareTag("arrosoir_1") && waterOn)
             {
                 Debug.Log("Arrosoir Réussit");
@@ -107,95 +100,165 @@ public class Greenhouse : MonoBehaviour
                 Debug.Log("Engrais Russit Réussit");
                 plante_1_etape_1.SetActive(false);
                 plante_1_etape_2.SetActive(true);
-                light = true;
+                light_square = true;
             }
         }
 
-
         if (stepOnePlant)
         {
-            // Vérifier si l'objet a le tag "Arrosoir"
             if (selectedObject.CompareTag("arrosoir_1") && waterOn)
             {
                 Debug.Log("Arrosoir Réussit");
                 plante_2_etape_0.SetActive(false);
                 plante_2_etape_1.SetActive(true);
                 waterOn = false;
-
             }
             if (selectedObject.CompareTag("engrais_2") && plante_2_etape_1)
             {
                 Debug.Log("Engrais Russit Réussit");
                 plante_2_etape_1.SetActive(false);
                 plante_2_etape_2.SetActive(true);
-                light = true;
+                light_square = true;
+            }
+        }
+
+        if (stepTwoPlant)
+        {
+            if (selectedObject.CompareTag("arrosoir_1") && waterOn)
+            {
+                Debug.Log("Arrosoir Réussit");
+                plante_3_etape_0.SetActive(false);
+                plante_3_etape_1.SetActive(true);
+                waterOn = false;
+            }
+            if (selectedObject.CompareTag("engrais_3") && plante_3_etape_1)
+            {
+                Debug.Log("Engrais Russit Réussit");
+                plante_3_etape_1.SetActive(false);
+                plante_3_etape_2.SetActive(true);
+                light_square = true;
             }
         }
     }
 
     private IEnumerator Timerchangeplant()
     {
-        // Wait for 3 seconds
         yield return new WaitForSeconds(3f);
 
-        // After 3 seconds, change the objects as required
-        plante_1_etape_3.SetActive(false);
-        plante_2_etape_0.SetActive(true);
-        Debug.Log("Plante 1 étape 3 est maintenant désactivée et Plante 2 étape 0 est activée");
-        light_green.SetActive(false);
+        light_square = false;
         lightchange = false;
+        light_green.SetActive(false);
+
+        if (stepOnePlant)
+        {
+            plante_1_etape_3.SetActive(false);
+            plante_2_etape_0.SetActive(true);
+            Debug.Log("Plante 1 étape 3 est maintenant désactivée et Plante 2 étape 0 est activée");
+        }
+
+        if (stepTwoPlant)
+        {
+            plante_2_etape_3.SetActive(false);
+            plante_3_etape_0.SetActive(true);
+            Debug.Log("Plante 1 étape 3 est maintenant désactivée et Plante 2 étape 0 est activée");
+        }
+
     }
+
 
     private IEnumerator Timerchangelight()
     {
-        // Wait for 3 seconds
         yield return new WaitForSeconds(3f);
-        light_red.SetActive(false);
-        light_green.SetActive(true);
+        light_square = true;
         Debug.Log("Plante 1 étape 3 est maintenant désactivée et Plante 2 étape 0 est activée");
         lightchange = true;
-    }
+
+
+
+        light_red.SetActive(false);
+        light_green.SetActive(true);
+}
 
     public void Update()
     {
-
-        if (stepZeroPlant && light)
+        if (stepZeroPlant && light_square)
         {
             if (lightchange)
             {
-                light = false;
+                light_square = false;
                 plante_1_etape_2.SetActive(false);
                 plante_1_etape_3.SetActive(true);
+                Debug.Log("Light Réussit ! Plante 1 réussit");
                 light_teller_green_1.SetActive(true);
                 light_teller_red_1.SetActive(false);
-                Debug.Log("Light Réussit ! Plante 1 réussit");
                 stepOnePlant = true;
                 stepZeroPlant = false;
                 StartCoroutine(Timerchangeplant());
             }
-
         }
 
+        if (stepOnePlant && light_square)
+        {
+            if (lightchange)
+            {
+                light_square = false;
+                plante_2_etape_2.SetActive(false);
+                plante_2_etape_3.SetActive(true);
+                light_teller_green_2.SetActive(true);
+                light_teller_red_2.SetActive(false);
+                Debug.Log("Light Réussit ! Plante 2 réussit");
+                stepTwoPlant = true;
+                stepOnePlant = false;
+                StartCoroutine(Timerchangeplant());
+            }
+        }
+
+        if (stepTwoPlant && light_square)
+        {
+            if (lightchange)
+            {
+                light_square = false;
+                plante_3_etape_2.SetActive(false);
+                plante_3_etape_3.SetActive(true);
+                light_teller_green_3.SetActive(true);
+                light_teller_red_3.SetActive(false);
+                Debug.Log("Light Réussit ! Plante 2 réussit");
+                stepThreePlant = true;
+                stepTwoPlant = false;
+            
+            }
+        }
+    
+        if(stepThreePlant)
+        {
+            Debug.Log("JEU FINI");
+        }
     }
+
     public void Light()
     {
-        if (stepZeroPlant && light)
+
+        if (stepZeroPlant && light_square)
         {
-            light_red.SetActive(true);
+
+            light_square = false;
             StartCoroutine(Timerchangelight());
+            light_red.SetActive(true);
 
         }
 
-        if (stepOnePlant && light)
+        if (stepOnePlant && light_square)
         {
-            light = false;
-            plante_2_etape_2.SetActive(false);
-            plante_2_etape_3.SetActive(true);
-            light_teller_green_2.SetActive(true);
-            light_teller_red_2.SetActive(false);
-            Debug.Log("Light Réussit ! Plante 2 réussit");
-            stepTwoPlant = true;
-            stepOnePlant = false;
+            light_square = false;
+            light_red.SetActive(true);
+            StartCoroutine(Timerchangelight());
+        }
+
+        if (stepTwoPlant && light_square)
+        {
+            light_square = false;
+            light_red.SetActive(true);
+            StartCoroutine(Timerchangelight());
         }
     }
 }
