a6f6d7b3dd58bf2f3118608dd8b9bd48f1e49d6a
Justinersu <justineroussoeau@gmail.com>
2024-12-02 21:46:10 -0500

    timer

--- eco6teme-tp3/Assets/Scripts/Timer.cs
+++ eco6teme-tp3/Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     float elapsedTime;
+    public static float recordedTime;
+    public static float bestTime = float.MaxValue;
 
     // Update is called once per frame
     void Update()
@@ -16,5 +18,18 @@ public class Timer : MonoBehaviour
         int seconds = Mathf.FloorToInt(elapsedTime % 60);
 
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void RecordTime()
+    {
+        recordedTime = elapsedTime;
+        Debug.Log("Recorded Time: " + recordedTime);
+
+        // Update the score if the new time is better
+        if (recordedTime < bestTime)
+        {
+            bestTime = recordedTime;
+            Debug.Log("New Best Time: " + bestTime);
+        }
     }
 }
---
+++ eco6teme-tp3/Assets/Scripts/displayScore.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class displayScore : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI recordedTimeText;
+    [SerializeField] TextMeshProUGUI bestTimeText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        float bestTime = Timer.bestTime;
+
+        if (Timer.recordedTime <= bestTime)
+        {
+        int minutes = Mathf.FloorToInt(Timer.recordedTime / 60);
+        int seconds = Mathf.FloorToInt(Timer.recordedTime % 60);
+        recordedTimeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        }
+
+        if (bestTime < float.MaxValue)
+        {
+            int bestMinutes = Mathf.FloorToInt(bestTime / 60);
+            int bestSeconds = Mathf.FloorToInt(bestTime % 60);
+            bestTimeText.text = string.Format("{0:00}:{1:00}", bestMinutes, bestSeconds);
+        }
+        else
+        {
+            bestTimeText.text = "No best time recorded yet.";
+        }
+
+    }
+}
